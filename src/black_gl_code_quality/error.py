3ac0d0461a29ae079757b40950d4983477efd15a
Michael Vieira <michael.vieira@gandi.net>
2023-12-08 15:04:46 +0100

    Add missing required informations

--- src/black_gl_code_quality/error.py
+++ src/black_gl_code_quality/error.py
@@ -9,6 +9,10 @@ def generate_error(path: str, severity: str) -> Mapping[str, Any]:
     err = {
         "type": "issue",
         "description": "Black would reformat",
+        "check_name": "inconsistent-format",
+        "category": [
+            "Style",
+        ],
         "location": {
             "lines": {"begin": 1, "end": 1},
             "path": path,
